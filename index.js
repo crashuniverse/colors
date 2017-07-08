711af29066a1bfda5e06c0087ccb0f747c6eda91
Priya Ranjan Singh <mail@priyaranjan.net>
2017-07-08 19:15:46 +0530

    add handler to cycle btwn 10 colors

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', loadAllFunctions, false);
+
+function loadAllFunctions() {
+  activateBlockClick();
+}
+
+function activateBlockClick() {
+  var block = document.querySelector('.block');
+  block.addEventListener('click', switchClass, false);
+}
+
+function switchClass() {
+  console.log('block is clicked');
+  var classes = ['black', 'red', 'yellow', 'green', 'blue', 'white', 'gray', 'orange', 'pink', 'purple'];
+  var block = document.querySelector('.block');
+  var activeClass = block.classList[1];
+  var index = classes.indexOf(activeClass);
+  block.classList.remove(activeClass);
+  var classToAdd = classes[index + 1];
+  block.classList.add(classToAdd);
+}
