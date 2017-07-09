199db7cb92b5351224c4dc4345aa006e9959b565
Priya Ranjan Singh <mail@priyaranjan.net>
2017-07-09 15:53:14 +0530

    reward user on completion of all colors

--- index.js
+++ index.js
@@ -6,18 +6,34 @@ function loadAllFunctions() {
 
 function activateBlockClick() {
   var block = document.querySelector('.block');
-  block.addEventListener('click', switchClass, false);
+  block.addEventListener('click', proceed, false);
+}
+
+function proceed() {
+  var isLastColor = switchClass();
+  if (isLastColor) {
+    reward();
+  }
 }
 
 function switchClass() {
   var classes = ['black', 'red', 'yellow', 'green', 'blue', 'white', 'gray', 'orange', 'pink', 'purple'];
+  var isLastClass = false;
   var block = document.querySelector('.block');
   var activeClass = block.classList[1];
   var index = classes.indexOf(activeClass);
   var newIndex = index + 1;
   if (index === classes.length - 1) {
-    newIndex = 0;
+    isLastClass = true;
+    return isLastClass;
   }
   block.classList.remove(activeClass);
   block.classList.add(classes[newIndex]);
+  return isLastClass;
+}
+
+function reward() {
+  var catContainer = document.querySelector('.cat');
+  catContainer.classList.remove('hide');
+  catContainer.classList.add('show');
 }
